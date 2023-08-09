1307c0bfc893b63ba943d85118600339bfc98ac4
Viktor Ekström <viktorekstrom1998@gmail.com>
2023-08-10 00:46:54 +0200

    added backup script and updated init script

---
+++ sql/backup_data.sql
@@ -0,0 +1,1 @@
+\COPY companies TO '{path}' WITH CSV;
--- sql/init.sql
+++ sql/init.sql
@@ -2,4 +2,18 @@ CREATE TABLE IF NOT EXISTS companies (
     id SERIAL PRIMARY KEY,
     company_name varchar,
     company_symbol varchar(32)
-)
+);
+CREATE TABLE IF NOT EXISTS company_data (
+    id SERIAL PRIMARY KEY,
+    company_name varchar,
+    company_symbol varchar(32),
+    industry varchar(100),
+    sector varchar(100),
+    full_time_employees int,
+    return_on_assets float,
+    return_on_equity float,
+    market_cap bigint,
+    current_price float,
+    trailing_eps float,
+    current_pe float
+);
