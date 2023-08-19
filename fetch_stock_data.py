cc832f5a1f1ab95a2f0b5f8834a664ff23664abb
Viktor Ekström <viktorekstrom1998@gmail.com>
2023-08-19 19:41:48 +0200

    refactored code

--- fetch_stock_data.py
+++ fetch_stock_data.py
@@ -1,120 +1,89 @@
-import psycopg2, configparser
 import yfinance as yf
-import pandas as pd
+import pandas as pd 
 import numpy as np
-import subprocess
+from utilities.db_utils import connect_to_database, look_up_dimension_keys_for_stock_data
+from utilities.stock_utils import compute_ema
 
-config = configparser.ConfigParser()
-config.read('.cfg')
-try:
-    # Connect to the PostgreSQL database
-    connection = psycopg2.connect(
-        dbname=config['POSTGRES']['POSTGRES_DB'],
-        user=config['POSTGRES']['POSTGRES_USER'],
-        password=config['POSTGRES']['POSTGRES_PASSWORD'],
-        host=config['POSTGRES']['HOST']
-    )
-    print("Connection successful.")
-except Exception as e:
-    print("Connection unsuccessful. Error: " + str(e))
-
+connection = connect_to_database()
 cursor = connection.cursor()
 
-df = pd.read_csv("backups/companies.csv",usecols=[0, 1, 2], header=None)
-df.columns = ['ID', 'Company Name', 'Symbol']
+# Execute a SELECT query on the desired column
+cursor.execute("SELECT symbol FROM companies;")
 
-def check_if_value_exists(company_symbol):
-    query = """SELECT COUNT(*) FROM companies WHERE company_symbol = %s"""
-    cursor.execute(query, (company_symbol))
-    results = bool(cursor.fetchone()[0])
-    return results
+# Fetch all rows from the result
+symbols = cursor.fetchall()
 
-truncate_query = """TRUNCATE TABLE company_data"""
-cursor.execute(truncate_query)
-connection.commit()
-symbol_errors = []
-for symbol in df.Symbol:
+for symbol in symbols:
+    symbol = symbol[0]
     try:
-        stock = yf.Ticker(symbol)
+        stock = yf.Ticker(symbol).info
+        stock_history = yf.Ticker(symbol).history(period='max')
+        historical_data = np.array(stock_history.Close)
         print("Looking at " + symbol)
-        try:
-            company_name = stock.info['shortName']
-        except:
-            company_name = None
-        
-        try:
-            symbol = stock.info['symbol']
-        except:
-            symbol = None
+
+        company_id, industry_id, sector_id = look_up_dimension_keys_for_stock_data(cursor, stock_info=stock)
 
         try:
-            industry = stock.info['industry']
-        except:
-            industry = None
-        
-        try:
-            sector = stock.info['sector']
-        except:
-            sector = None
-        
-        try:
-            fte = stock.info['fullTimeEmployees']
-        except:
-            fte = None
-
-        try:
-            return_on_assets = stock.info['returnOnAssets']
+            return_on_assets = stock['returnOnAssets']
         except:
             return_on_assets = None
         
         try:
-            return_on_equity = stock.info['returnOnEquity']
+            return_on_equity = stock['returnOnEquity']
         except:
             return_on_equity = None
         
         try:
-            market_cap = stock.info['marketCap']
+            market_capitalization = stock['marketCap']
         except:
-            market_cap = None
+            market_capitalization = None
         
         try:
-            current_price = stock.info['currentPrice']
+            price = stock['currentPrice']
         except:
-            current_price = None
+            price = None
         
         try:
-            trailing_earnings_per_share = stock.info['trailingEps']
+            ema_34 = compute_ema(data=historical_data, smoothing_factor=2, days_back=34)[-1]
+        except:
+            ema_34 = None
+
+        try:
+            ema_200 = compute_ema(data=historical_data, smoothing_factor=2, days_back=200)[-1]
+        except:
+            ema_200 = None
+
+        try:
+            trailing_earnings_per_share = stock['trailingEps']
         except:
             trailing_earnings_per_share = None
         
         try:
-            current_pe = current_price/trailing_earnings_per_share
+            price_earnings_ratio = round(price/trailing_earnings_per_share, 2)
         except:
-            current_pe = None
-        
-        try:
-            business_summary = stock.info['longBusinessSummary']
-        except:
-            business_summary = None
+            price_earnings_ratio = None
+
         
         query = """
-                INSERT INTO company_data 
-                (company_name, company_symbol, industry, sector, full_time_employees, return_on_assets, 
-                return_on_equity, market_cap, current_price, trailing_eps, current_pe, business_summary)
-                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                INSERT INTO stock_data 
+                (price, ema_34, ema_200, return_on_assets, 
+                return_on_equity, trailing_earnings_per_share,
+                price_earnings_ratio, market_capitalization,
+                company_id, industry_id, sector_id)
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
                 """
 
-        cursor.execute(query, (company_name, symbol, industry, sector, fte, return_on_assets, return_on_equity, market_cap, current_price, trailing_earnings_per_share, current_pe, business_summary))
+        cursor.execute(query, (price, ema_34, ema_200, return_on_assets, return_on_equity, 
+                               trailing_earnings_per_share, price_earnings_ratio, 
+                               market_capitalization, company_id, industry_id, sector_id))
         
-        print("Insertion of data from stock " + company_name + " completed.")
+        print(f"Insertion of data from stock {symbol} completed.")
     except Exception as e:
-        symbol_errors.append(symbol)
-        print("Error: " + str(e))
+        print("Error inserting data.")
+        raise e
 
 connection.commit()
+
 cursor.close()
 connection.close()
-
-subprocess.run(['python3', 'backup_data.py'], check=True)
-
-print("Finished.")
+print("Insertion complete.")
