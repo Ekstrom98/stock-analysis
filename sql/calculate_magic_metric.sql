b2e2993e431c18c61eaf28dac8588c6674c15b69
Viktor Ekström <viktorekstrom1998@gmail.com>
2023-09-03 15:34:03 +0200

    added script for calculating the 'magic score'

---
+++ sql/calculate_magic_metric.sql
@@ -0,0 +1,14 @@
+WITH screened_table AS (
+    SELECT *
+    FROM stock_analytics
+    WHERE (price_earnings_ratio IS NOT NULL) AND (price_earnings_ratio > 0)
+    AND (return_on_assets IS NOT NULL) AND (return_on_assets > 0) 
+), 
+magic_table AS (
+    SELECT fetched_at, price, ema_34, ema_200, return_on_assets, return_on_equity, price_earnings_ratio,
+    market_capitalization, name, symbol,
+    DENSE_RANK() OVER (ORDER BY price_earnings_ratio DESC) pe_score,
+    DENSE_RANK() OVER (ORDER BY return_on_assets) roa_score
+    FROM screened_table
+)
+SELECT *, (pe_score+roa_score) magic_score FROM magic_table;
